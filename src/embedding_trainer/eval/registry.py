c1e41d3e0b5776453b3c53491317cba8954fe5cf
Ermo Wei <nosyndicate@gmail.com>
2026-02-22 00:52:37 -0800

    refactor: remove redundant evaluation submodule
    - Delete src/embedding_trainer/eval/ directory and all its contents
    - Remove outdated BaseEvalTask, MLMEvalTask, and model_loader
    - Consolidate evaluation logic within the unified BaseTrainer and BaseTask framework

--- src/embedding_trainer/eval/registry.py
+++
@@ -1,7 +0,0 @@
-"""Registry for evaluation tasks."""
-
-from __future__ import annotations
-
-from embedding_trainer.utils.registry import Registry
-
-EVAL_TASK_REGISTRY = Registry("eval_task")
--- src/embedding_trainer/eval/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Evaluation tasks."""
-
-# Import tasks to register them
-from embedding_trainer.eval.tasks.mlm import MLMEvalTask
-
-__all__ = ["MLMEvalTask"]
